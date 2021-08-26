97ee53ef0287cbff45995be358e6e978ff82f9dc
Ermiyas Zeleke <78529847+ermi2021@users.noreply.github.com>
2021-08-26 23:22:24 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Starting point
+ *main - Start point
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        printf("with proper grammer, but the outcome is a piece of art,\n");
-        return (0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
